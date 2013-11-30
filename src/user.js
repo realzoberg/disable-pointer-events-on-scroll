fce4343ac1c3977c259a8813afc31fb1fd189c38
Zoberg <v.zoberg.dev@gmail.com>
2013-11-30 05:30:19 +0400

    Initial commit
    Добавление к body класса с pointer-events: none при скролле

---
+++ src/user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name disable-pointer-events-on-scroll
+// @description 
+// @author Ruslan Login
+// @license MIT
+// @version 1.0
+// @include http://habrahabr.ru/*
+// @match http://habrahabr.ru/*
+// ==/UserScript==
+
+
+
+(function (window, undefined) { 
+
+var body = window.document.body,
+    timer;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+  
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },500);
+}, false);
+
+})(window);
